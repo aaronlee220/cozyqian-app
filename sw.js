30b007215b6ca54e88dd573784317f90172f89f9
aaronlee220 <255095aaron@gmail.com>
2026-06-17 14:53:33 +0000

    Init: CozyQian profit management app with Capacitor Android

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = "cozyqian-v1";
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(["./", "./manifest.json", "./icon-192.png", "./icon-512.png"]))
+  );
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
